6f99985cfc7688fbd2baf726b0eac27d4ce0d4b1
Kyle Pilon <pilonk@rpi.edu>
2023-07-02 23:27:00 -0400

    data has been loaded - new csv df

--- load_data.py
+++ load_data.py
@@ -1,27 +1,6 @@
-import os
-import json
 import pandas as pd
 
-def load_single_file(file_path):
-    with open(file_path) as f:
-        data = json.load(f)
-    return data
+# Load the DataFrame from the CSV file
+df = pd.read_csv('C:\\Users\\pilonk\\projects\\music-recommender\\data.csv')
 
-def load_all_files(directory_path):
-    data_list = []
-    for root, dirs, files in os.walk(directory_path):
-        for file in files:
-            if file.endswith('.json'):
-                file_path = os.path.join(root, file)
-                data = load_single_file(file_path)
-                data_list.append(data)
-    df = pd.DataFrame(data_list)
-    return df
-
-# Load all data
-df = load_all_files('C:\\Users\\pilonk\\projects\\music-recommender\\lastfm_subset')
-
-# Save the DataFrame to a CSV file
-df.to_csv('C:\\Users\\pilonk\\projects\\music-recommender\\data.csv', index=False)
-
-print("Data successfully saved to 'data.csv'")
+# Now you can work with the DataFrame 'df'
