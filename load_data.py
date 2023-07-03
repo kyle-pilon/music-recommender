c2deb334e881b73b596b3290854cc386584df592
Kyle Pilon <pilonk@rpi.edu>
2023-07-02 23:21:00 -0400

    data load

--- load_data.py
+++ load_data.py
@@ -2,29 +2,26 @@ import os
 import json
 import pandas as pd
 
-# Function to load a single JSON file
 def load_single_file(file_path):
     with open(file_path) as f:
         data = json.load(f)
     return data
 
-# Function to load all JSON files in a directory
 def load_all_files(directory_path):
     data_list = []
-    # os.walk generates the file names in a directory tree
     for root, dirs, files in os.walk(directory_path):
         for file in files:
-            # check if file is a JSON file
             if file.endswith('.json'):
                 file_path = os.path.join(root, file)
                 data = load_single_file(file_path)
                 data_list.append(data)
-    # Convert the list of dictionaries into a DataFrame
     df = pd.DataFrame(data_list)
     return df
 
 # Load all data
 df = load_all_files('C:\\Users\\pilonk\\projects\\music-recommender\\lastfm_subset')
 
-# Print the first few rows of the DataFrame
-print(df.head())
+# Save the DataFrame to a CSV file
+df.to_csv('C:\\Users\\pilonk\\projects\\music-recommender\\data.csv', index=False)
+
+print("Data successfully saved to 'data.csv'")
